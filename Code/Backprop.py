439bd5239d6ff2025f263ad9d1827a78f70787dd
kiwi2kiwi <yan!git13>
2023-12-03 12:51:40 +0100

    Pre "sensory confirmation"

--- Code/Backprop.py
+++ Code/Backprop.py
@@ -1,57 +1,82 @@
+import random
 from Neuron import *
 
 input = [1.5]
-weights = [0.8,0.2]
+weights = [0.8,0.2,0.5]
 target = [0.5]
 
 n1 = Neuron()
-n2 = output_Neuron()
-inn = input_Neuron()
-#n1.children_connections.append([n2,0.8])
-inn.children_connections.append([n2,weights[0]])
-n1.parent_connections.append([inn,weights[0]])
-n1.children_connections.append([n2,weights[1]])
-n2.parent_connections.append([n1,weights[1]])
-#n1.parent_connections.append([inn,1])
+n2 = Neuron()
+i1 = input_Neuron()
+
+i1.children_connections.append([n1, weights[0]])
+n1.parent_connections.append([i1, weights[0]])
+n1.children_connections.append([n2, weights[1]])
+n2.parent_connections.append([n1, weights[1]])
+
+active_neurons = [n1,n2]
+'''
+weights = []
+layers = 2
+
+for i in range(layers):
+    weights.append(round(random.uniform(0,1),2))
+
+
+for i in range(layers):
+    n = Neuron
+    active_neurons.append(n)
+
+active_neurons.append(o1)
+'''
+#inn.children_connections.append([n2,weights[0]])
+#n2.parent_connections.append([inn,weights[0]])
+
+
 
 
 def error_function(pre,tar):
-    return (pre - tar)**2
+    return round((pre - tar)**2,3)
 
 def deriv_error_function(pre,tar):
     return 2*(pre - tar)
 
 def compute_error():
-    inn.set_input(input[0])
-    inn.activation()
-    n2.activation()
-    print(n2.output)
-    print("error: ", error_function(n2.output, target[0]))
+    i1.set_input(input[0])
+    active_neurons[-1].activation()
+    print("error: ", error_function(active_neurons[-1].activation(), target[0]))
 
-def update_weights(weight):
+def backprop():
+    compute_error()
     learning_rate = 0.1
 #    a_null_durch_w = n2.deri_activation()
 #    error_durch_a_null = deriv_error_function(n2.activation(),target[0])
 #    error_durch_w = a_null_durch_w * error_durch_a_null
-    n2.gradient_descent(deriv_error_function, target[0], learning_rate)
+    error_through_a_zero = deriv_error_function(active_neurons[-1].activation(), target[-1])
+    active_neurons[-1].gradient_descent(error_through_a_zero, learning_rate)
 
 #    new_weight = weight - (learning_rate * error_durch_w)
 
-
+def change_weights():
+    [n.change_weight() for n in active_neurons]
 #    n2.change_weight(new_weight)
 
 #    print("weight: ", new_weight, " adjust: ", (weight - new_weight))
 #    return new_weight
 
 
-compute_error()
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
-update_weights(weight)
+backprop()
+change_weights()
+backprop()
+change_weights()
+backprop()
+change_weights()
+backprop()
+change_weights()
+backprop()
+change_weights()
+backprop()
+change_weights()
+backprop()
+change_weights()
 print("end")
--- Code/Neuron.py
+++ Code/Neuron.py
@@ -7,43 +7,61 @@ class Neuron():
         self.children_connections = [] # closer to output
         self.signals = []
         self.output = 0
+        self.new_weight = 0
         print("Hey, im a neuron!")
 
-        def change_weight(self, w):
-            self.parent_connections[0][1] = w
-            self.parent_connections[0].children_connections[0][1] = w
+    def change_weight(self):
+        self.parent_connections[0][1] = self.new_weight
+        self.parent_connections[0][0].children_connections[0][1] = self.new_weight
 
-        def get_weight(self):
-            return self.parent_connections[0][1]
+    def get_weight(self):
+        return self.parent_connections[0][1]
 
-        def gradient_descent(self, error_durch_a_null, learning_rate):
-            a_durch_w = self.deri_activation()
-            #error_durch_a_null = deriv_error_function(self.activation(), target)
-            error_durch_w = a_durch_w * error_durch_a_null
-            new_weight = self.get_weight() - (learning_rate * error_durch_w)
-            print("weight: ", new_weight, " adjust: ", (self.get_weight() - new_weight))
-            self.change_weight(new_weight)
+    def gradient_descent(self, bis_hier, learning_rate):
+        #a_durch_w = self.deri_activation()
+        #error_durch_a_null = deriv_error_function(self.activation(), target)
 
-        def activation_function(self, z):
-            return z
+        ab_hier = self.a_null_a_eins() * bis_hier
+        self.parent_connections[0][0].gradient_descent(ab_hier, learning_rate)
 
-        #        return 1. / (1 + np.exp(-z))
 
-        def deri_activation_function(self, z):
-            return z
+        error_durch_w = self.a_null_w_null() * bis_hier
+        self.new_weight = self.get_weight() - (learning_rate * error_durch_w)
+        print("weight: ", round(self.new_weight,2), " adjust: ", round(self.new_weight - self.get_weight(),2))
 
-        def activation(self):
-            summation = 0
-            for neuron, weight in self.parent_connections:
-                summation += neuron.activation() * weight
-            self.output = self.activation_function(summation)
-            return self.output
 
-        def deri_activation(self):
-            summation = 0
-            for neuron, weight in self.parent_connections:
-                summation += neuron.deri_activation()
-            return self.deri_activation_function(summation)
+    def activation_function(self, z):
+        return z
+
+    #        return 1. / (1 + np.exp(-z))
+
+    def deri_activation_function(self, z):
+        return z
+
+    def activation(self):
+        summation = 0
+        for neuron, weight in self.parent_connections:
+            summation += neuron.activation() * weight
+        self.output = self.activation_function(summation)
+        return self.output
+
+    def deri_activation(self):
+        summation = 0
+        for neuron, weight in self.parent_connections:
+            summation += neuron.deri_activation()
+        return self.deri_activation_function(summation)
+
+    def a_null_w_null(self):
+        summation = 0
+        for neuron, weight in self.parent_connections:
+            summation += neuron.activation()
+        return self.deri_activation_function(summation)
+
+    def a_null_a_eins(self):
+        summation = 0
+        for neuron, weight in self.parent_connections:
+            summation += weight
+        return self.deri_activation_function(summation)
 
 
 
@@ -52,6 +70,9 @@ class input_Neuron():
         super(input_Neuron, self).__init__()
         self.children_connections = [] # closer to output
         self.output = 0
+
+    def gradient_descent(self, a, b):
+        pass
 
     def set_input(self, input):
         self.output = input
@@ -71,24 +92,29 @@ class output_Neuron():
     def __init__(self):
         super(output_Neuron, self).__init__()
         self.parent_connections = [] # closer to output
+        self.children_connections = [] # closer to output
+        self.signals = []
         self.output = 0
+        self.new_weight = 0
 
-    def change_weight(self, w):
-        self.parent_connections[0][1] = w
-        self.parent_connections[0][0].children_connections[0][1] = w
+    def change_weight(self):
+        self.parent_connections[0][1] = self.new_weight
+        self.parent_connections[0][0].children_connections[0][1] = self.new_weight
 
     def get_weight(self):
         return self.parent_connections[0][1]
 
-    def gradient_descent(self, deriv_error_function, target, learning_rate):
-        a_null_durch_w = self.deri_activation()
-        error_durch_a_null = deriv_error_function(self.activation(), target)
-        self.parent_connections[0][0].gradient_descent(error_durch_a_null, learning_rate)
+    def gradient_descent(self, bis_hier, learning_rate):
 
-        error_durch_w = a_null_durch_w * error_durch_a_null
-        new_weight = self.get_weight() - (learning_rate * error_durch_w)
-        print("weight: ", new_weight, " adjust: ", (self.get_weight() - new_weight))
-        self.change_weight(new_weight)
+
+
+        ab_hier = self.a_null_a_eins() * bis_hier
+        self.parent_connections[0][0].gradient_descent(ab_hier, learning_rate)
+
+        error_durch_w = self.a_null_w_null() * bis_hier
+        self.new_weight = self.get_weight() - (learning_rate * error_durch_w)
+        print("weight: ", round(self.new_weight,2), " adjust: ", round(self.new_weight - self.get_weight(),2))
+
 
     def activation_function(self,z):
         return z
@@ -108,4 +134,17 @@ class output_Neuron():
         summation = 0
         for neuron, weight in self.parent_connections:
             summation += neuron.deri_activation()
+        return self.deri_activation_function(summation)
+
+
+    def a_null_w_null(self):
+        summation = 0
+        for neuron, weight in self.parent_connections:
+            summation += neuron.activation()
+        return self.deri_activation_function(summation)
+
+    def a_null_a_eins(self):
+        summation = 0
+        for neuron, weight in self.parent_connections:
+            summation += weight
         return self.deri_activation_function(summation)
